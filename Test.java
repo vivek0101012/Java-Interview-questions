964b5c8f523a0d30cf52ab7945cfaf75283a1640
Vivek Nishad <vivekmmmut2027@gmail.com>
2026-06-02 10:32:51 +0530

    Initial commit

---
+++ Test.java
@@ -0,0 +1,41 @@
+
+// Abstraction - implemented through interfaces 
+
+interface  Payment {
+    void pay();
+}
+
+class  UPIPayment implements Payment{
+
+    public void pay(){
+    System.out.println("upi payment");
+    }
+}
+
+
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Payment p=new UPIPayment();
+        p.pay();
+    }
+}
+
+//1.### Why can we write
+
+// java
+// Payment p = new UPIPayment();
+// 
+
+// instead of  UPIPayment p = new UPIPayment()
+
+// Answer
+
+// The object being created is:
+// new UPIPayment()
+// which implements the Payment interface.
+// Therefore, a reference of type Payment can point to a UPIPayment object.
+// /The major advantage is that our code depends on the abstraction (Payment) rather than a specific implementation (UPIPayment)
+// the same can be implemented later by the creaditcartPayment etc.
