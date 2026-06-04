4bc49bf8d861555524d14df3ca88c744d91e547e
VIVEK NISHAD <vivekmmmut2027@gmail.com>
2026-06-04 13:08:05 +0530

    Update Test4.java

--- Test4.java
+++ Test4.java
@@ -29,13 +29,16 @@ public class Test4 {
 
 // explain what is run time binding ?
 
-// ans-> for instance methods java uses the dynamic dispatching mechanism in which the run time object is used to determine which variation of accessible methods (which are present in  the reference type variable) shoudld be used , this late binding to run time object is called run time bidning 
+// ans-> for instance methods java uses the dynamic dispatching mechanism in which the run time object is used to determine
+//which variation of accessible methods (which are present in  the reference type variable) shoudld be used , this late binding 
+//to run time object is called run time bidning 
 
 
 // explain the override annotation uses?
 
 // ans-> it serves two purposes->
-//1.result in compile error in case wrong method name or signature are used to override -- without this java would consider them new methods instead of overrding
+//1.result in compile error in case wrong method name or signature are used to override -- without this java
+//would consider them new methods instead of overrding
 
 //2. it explicits the developer intention to override the method , improving readablity ,avoding bugs etc.
 
@@ -49,12 +52,14 @@ public class Test4 {
 
 // 4 constructor start executing , since the class inherits another class so compiler adds a implicit super() to invoke parent constructor , 
 
-// 5 parent also has no constructor so compiler insert a no arg cosnt and intialize parent-- animal , and retunr control to dog which intializes and jvm return address to ref variable.
+// 5 parent also has no constructor so compiler insert a no arg cosnt and intialize parent-- animal , and return
+//control to dog which intializes and jvm return address to ref variable.
 
 //7.since a.sound() has ref of animal so allowed methods are the methdos in animal class overriden or parents own implementation
 
-// 8 . since there is overriden variation compiler start executing sound , which has first line super.sound(), here super refer to parent animal so it runs the parent variation pritning animal sound amd return control to child dog 
+// 8 . since there is overriden variation compiler start executing sound , which has first line super.sound(),
+//here super refer to parent animal so it runs the parent variation pritning animal sound amd return control to child dog 
 
-//9. now child  pring dog barking.
+//9. now child  print dog barking.
 
 
