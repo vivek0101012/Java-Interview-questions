e49c212486c931b0b37922f3a6050d005ae9ff6d
VIVEK NISHAD <vivekmmmut2027@gmail.com>
2026-06-09 14:28:35 +0530

    Update Test9.java

--- Test9.java
+++ Test9.java
@@ -1,11 +1,11 @@
 // 1. How the method resoultion happens in java  for non static methods ?
 
-//ans-> for non static methdos the  reference varaible 
-//  decides what methods are accescible during the compile time it self -> it is called compile time bidning or early binding
+//ans-> for non static methods the  reference variable 
+//  decides what methods are accessible during the compile time it self -> it is called compile time bidning or early binding or static binding
 
-// during the run time , the run time object decides which variation of the method will eb exactly executed--> run time binding / dynamic dispatch mechanism. 
+// during the run time , the run time object decides which variation of the allowed method will be exactly executed--> run time binding / dynamic dispatch mechanism. 
 
-//Note- > variables are always resolved using the reference variables 
+//Note- > instance variables are always resolved using the reference variables 
 // so they dont participate in run time polymorphism.--> called : Variable Shadowing (or Hiding).
 
 class t1{
