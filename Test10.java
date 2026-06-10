6e3cb9fc26edbcb47007f933e3a6e8f9bd742ea7
Vivek Nishad <vivekmmmut2027@gmail.com>
2026-06-10 19:25:10 +0530

    updated

--- Test10.java
+++ Test10.java
@@ -2,34 +2,34 @@
 //How would you design this key class to ensure that it works correctly in a HashMap?
 
 
-// > A custom object can be used as a key in a HashMap, provided it is designed correctly. A good custom key should:
-// > 
-// > 1. Be immutable
-// > 2. Override equals
-// > 3. Override hashCode().
-// > 4. Use the significant fields for both equality and hash computation.
-// > 
-// > HashMap internally uses the hashCode() of the key to determine the bucket in which the
+//  A custom object can be used as a key in a HashMap, provided it is designed correctly. A good custom key should:
+//  
+//  1. Be immutable
+//  2. Override equals
+//  3. Override hashCode().
+//  4. Use the significant fields for both equality and hash computation.
+//  
+//  HashMap internally uses the hashCode() of the key to determine the bucket in which the
 //entry should be stored. Later, during retrieval, it again computes the hashCode to locate the 
 //bucket and finally uses equals() to identify the exact key among possible collisions.
-// > 
-// > Therefore, if equals() is overridden but hashCode() is not, two logically equal objects may produce
+//  
+//  Therefore, if equals() is overridden but hashCode() is not, two logically equal objects may produce
 //different hash codes and be placed into different buckets, causing retrieval failures and violating the HashMap contract.
-// > 
-// > The key should also be immutable. If a field affecting hashCode() or equals() is modified after insertion,
+//  
+//  The key should also be immutable. If a field affecting hashCode() or equals() is modified after insertion,
 //the object remains stored in its original bucket while future lookups compute a different hash and search 
 //another bucket. As a result, operations like get() or remove() may return null even though the entry exists.
-// > 
-// > Finally, the contract is:
-// > 
-// >  Equal objects ⇒ same hashCode
-// >  Same hashCode ⇏ equal objects (collisions are possible)
-// > 
-// > Hence, for custom HashMap keys, we should always override both equals() and hashCode() and preferably make the key immutable.
-// >
+//  
+//  Finally, the contract is:
+//  
+//   Equal objects ⇒ same hashCode
+//   Same hashCode ⇏ equal objects (collisions are possible)
+//  
+//  Hence, for custom HashMap keys, we should always override both equals() and hashCode() and preferably make the key immutable.
+// 
 
 
-//overriden code example -- >
+//overriden code example -- 
 
 @override
 public Boolean equals (object obj){
