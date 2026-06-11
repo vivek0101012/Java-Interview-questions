689a2b6d072eb16d4130aa64d2f1404d5b2a7489
Vivek Nishad <vivekmmmut2027@gmail.com>
2026-06-11 15:46:40 +0530

    final question set on string

--- Ex.java
+++ Ex.java
@@ -1,44 +1,24 @@
-
-class dog implements Cloneable{
-    int x=10;
-    @Override
-    public Object clone()
-    throws CloneNotSupportedException {
-      return (dog) super.clone();
-    }
-
-}
-class cat {
-    int x=10;
-    @Override 
-    public  boolean  equals(Object obj){
-        if (this==obj) return true;
-        if (obj instanceof  dog){
-            dog a = (dog) obj;
-            if (this.x==a.x) return true;
-        }
-        
-        return false;
-        
-    }
-}
+import java.util.LinkedHashMap;
 
 public class Ex{
 
     public static void main(String[] args) {
         
-        dog d= new dog();
-     
-        try {
-            
-                    dog d1=(dog) d.clone();
+     LinkedHashMap<Integer,String> map=new LinkedHashMap<>(16,0.75f,true);
+      map.put(1,"A");
+        map.put(21,"B");
+        map.put(23,"C");
+        map.put(24,"D");
+        map.put(25,"E");
+        System.out.println(map);
+        map.get(23);
+        map.get(24);
+        //now  24 becomes most recently used so moves to back and 23 is secnd most recently used so it get 2nd last position hence the output  is
 
-                    System.out.println(d1.x);
-        } catch (Exception e) {
-
-                    
-        }
-
+       // iteration using the for each loop
+    
+        map.forEach((Integer key, String value)-> System.out.println(key +" " +value));
+        
 
     }
 }
