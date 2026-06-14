7a46c68dde116bbe390efcc2ebd970a84e02e73e
VIVEK NISHAD <vivekmmmut2027@gmail.com>
2026-06-14 21:47:31 +0530

    Update Test1.java

--- Generics.java/Test1.java
+++ Generics.java/Test1.java
@@ -2,7 +2,9 @@
 
 // Answer
 
-// Type Erasure is the process by which the Java compiler removes all generic type information during compilation. The compiler replaces type parameters with their bounds or with Object if they are unbounded. Therefore, Generics provide compile-time type safety, but generic type information is not available at runtime.
+// Type Erasure is the process by which the Java compiler removes all generic type information during compilation.
+//The compiler replaces type parameters with their bounds or with Object if they are unbounded. Therefore, Generics 
+//provide compile-time type safety, but generic type information is not available at runtime.
 
 // 121 Suppose you need to overload a method to handle both a
 // List<Integer> and a List<Double>. Can you overload methods
@@ -23,7 +25,8 @@
 // These methods will cause a compile-time error because after type erasure,
 // both methods would look like:
 
-// both list  converts to raw type list hence the same  method signature —> causes compile time issues as no two methods in same class can be over loaded with same signature .
+// both list  converts to raw type list hence the same  method signature —>
+//causes compile time issues as no two methods in same class can be over loaded with same signature .
 // public
 
 // 
@@ -40,7 +43,10 @@
 // For example, T[] would be treated as Object[], which could allow inserting
 // elements of any type, breaking the type safety of the array
 
-// Arrays require runtime type information, whereas Generics lose their type information due to Type Erasure. As a result, generic arrays would lose type safety and could lead to runtime `ClassCastException`. Therefore Java prohibits creating arrays of generic types.
+// Arrays require runtime type information, whereas Generics lose their type
+//information due to Type Erasure. As a result, generic arrays would lose type 
+//safety and could lead to runtime `ClassCastException`. Therefore Java prohibits 
+//creating arrays of generic types.
 
 public class Test1 {
     
